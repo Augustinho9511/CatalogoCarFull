5e32e881b4bf3c340d3081f84cbe63415dce6e80
Pedro Augusto <pedroaugusto159159@gmail.com>
2026-01-15 17:31:01 +0100

    Corrigindo pastas e subindo projeto completo

--- CatalogodeCarro/src/main/java/CatalogodeCarro/respository/CarResposity.java
+++
@@ -1,7 +0,0 @@
-package CatalogodeCarro.respository;
-
-import CatalogodeCarro.model.entity.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarResposity extends JpaRepository<Car, Long> {
-}
